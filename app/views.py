395194fa3476e2ca7f180ea2ce57a28d16086b0c
wangechimk <wangechik.michelle@gmail.com>
2021-04-12 16:26:16 +0300

    add a route decorator and create a movie.html file

--- app/views.py
+++ app/views.py
@@ -10,3 +10,11 @@ def index():
     """
     message = 'Hello World'
     return render_template('index.html',message = message)
+
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+
+    """
+    View movie page function that returns the movie details page and its data
+    """
+    return render_template('movie.html',id = movie_id)    
