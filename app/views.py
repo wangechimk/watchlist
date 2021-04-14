413e729f895d84512be9198c959ca990f7ca4a6a
wangechimk <wangechik.michelle@gmail.com>
2021-04-14 23:05:08 +0300

    create wtf forms

--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from app import app
 from .request import get_movies,get_movie
 from .request import get_movies,search_movie
 from flask import render_template,request,redirect,url_for
+from .forms import ReviewForm
+from .models import review
+Review = review.Review
 
 # Views
 
@@ -58,5 +61,20 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)    
    
  
