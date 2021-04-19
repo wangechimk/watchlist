4f428df46c5b5a0cac076916c44326c57976632f
wangechimk <wangechik.michelle@gmail.com>
2021-04-19 11:23:46 +0300

    creatingthe first model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
-class Movie:
-    '''
-    Movie class to define Movie Objects
-    '''
+from . import db
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
-        self.id =id
+
+class Movie:
+    """
+    Movie class to define Movie Objects
+    """
+
+    def __init__(self, id, title, overview, poster, vote_average, vote_count):
+        self.id = id
         self.title = title
         self.overview = overview
         self.poster = "https://image.tmdb.org/t/p/w500/" + poster
@@ -12,28 +15,24 @@ class Movie:
         self.vote_count = vote_count
 
 
-
 class Review:
-
     all_reviews = []
 
-    def __init__(self,movie_id,title,imageurl,review):
+    def __init__(self, movie_id, title, imageurl, review):
         self.movie_id = movie_id
         self.title = title
         self.imageurl = imageurl
         self.review = review
 
-
     def save_review(self):
         Review.all_reviews.append(self)
-
 
     @classmethod
     def clear_reviews(cls):
         Review.all_reviews.clear()
 
     @classmethod
-    def get_reviews(cls,id):
+    def get_reviews(cls, id):
 
         response = []
 
@@ -42,3 +41,12 @@ class Review:
                 response.append(review)
 
         return response
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -1,14 +1,15 @@
-from app import create_app
-from flask_script import Manager,Server
-
-
+from app import create_app, db
+from flask_script import Manager, Server
+from app.models import User
 
 # ...
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
+
+
 @manager.command
 def test():
     """Run the unit tests."""
@@ -16,6 +17,14 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
+
+if __name__ == '__main__':
+    manager.run()
 
 if __name__ == '__main__':
     manager.run()
