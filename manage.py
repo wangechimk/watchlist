dcacfb6b943c731b302d5481bd2636569eaf9951
wangechimk <wangechik.michelle@gmail.com>
2021-04-19 11:38:56 +0300

    definingthe user relationship with the application

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User
+from app.models import User, Role
 
 # ...
 # Creating app instance
@@ -20,11 +20,8 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Role=Role)
 
-
-if __name__ == '__main__':
-    manager.run()
 
 if __name__ == '__main__':
     manager.run()
