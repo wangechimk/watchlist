3756c94531260919f450f22bfcf97c944eb2b8ad
wangechimk <wangechik.michelle@gmail.com>
2021-04-19 21:22:08 +0300

    add the load user callback

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
 from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class Movie:
@@ -44,12 +51,13 @@ class Review:
         return response
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255), index=True)
+    email = db.Column(db.String(255), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):
