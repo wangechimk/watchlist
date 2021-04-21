9064bfa43477dc9ba886c5a5e82fc77f04596859
wangechimk <wangechik.michelle@gmail.com>
2021-04-21 23:53:46 +0300

    update the user profile

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
 
-    title = StringField('Review title',validators=[Required()])
+class ReviewForm(FlaskForm):
+    title = StringField('Review title', validators=[Required()])
     review = TextAreaField('Movie review', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
