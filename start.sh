3baafe3d676f30ea0f05004be1078332d3e0b82c
wangechimk <wangechik.michelle@gmail.com>
2021-04-23 11:22:40 +0300

    make some changes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MOVIE_API_KEY='1f5980b0dc895e1622c9eb23309a98db'
 export SECRET_KEY='1234'
 export MAIL_USERNAME=wangechik.michelle@gmail.com
-export MAIL_PASSWORD=Angelstar44
+export MAIL_PASSWORD=Potato
 
 python3.9 manage.py server
